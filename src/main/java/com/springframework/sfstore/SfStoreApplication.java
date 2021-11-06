ac24eced7361abe64a836efd75b48c6d4177375b
karthik <Karthik.Samaganam@walmart.com>
2021-11-06 12:00:13 +0530

    Initial project

---
+++ src/main/java/com/springframework/sfstore/SfStoreApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.sfstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/sfstore/SfStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
